badbd4b9490c3e220cadee52ca9fe2d09893a300
sumit raina <sumit.raina1992@gmail.com>
2020-12-25 14:26:34 +0530

    update

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
 create table AUTHOR (name varchar(50) , book varchar(50));
 
-insert into AUTHOR VALUES ('sumit', '1');
-insert into AUTHOR VALUES ('sumit', '2');
-insert into AUTHOR VALUES ('sumit', '3');
-insert into AUTHOR VALUES ('sumit', '4');
-insert into AUTHOR VALUES ('sumit', '5');
-insert into AUTHOR VALUES ('sumit', '6');
+insert into AUTHOR VALUES ('sam', '1');
+insert into AUTHOR VALUES ('sam', '2');
+insert into AUTHOR VALUES ('sam', '3');
+insert into AUTHOR VALUES ('sam', '4');
+insert into AUTHOR VALUES ('sam', '5');
+insert into AUTHOR VALUES ('sam', '6');
 
-insert into AUTHOR VALUES ('sahil', 'mere tatti');
-insert into AUTHOR VALUES ('sahil', 'khatti tatti');
-insert into AUTHOR VALUES ('sahil', 'tatti ki yaad');
+insert into AUTHOR VALUES ('jane', 'book a');
+insert into AUTHOR VALUES ('jane', 'book b');
+insert into AUTHOR VALUES ('jane', 'book c');
