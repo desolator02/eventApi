bc06b3ae9d479b36f11d1cba93130b38e77a86d1
sumit raina <sumit.raina1992@gmail.com>
2020-12-25 14:22:05 +0530

    added sql import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+create table AUTHOR (name varchar(50) , book varchar(50));
+
+insert into AUTHOR VALUES ('sumit', '1');
+insert into AUTHOR VALUES ('sumit', '2');
+insert into AUTHOR VALUES ('sumit', '3');
+insert into AUTHOR VALUES ('sumit', '4');
+insert into AUTHOR VALUES ('sumit', '5');
+insert into AUTHOR VALUES ('sumit', '6');
+
+insert into AUTHOR VALUES ('sahil', 'mere tatti');
+insert into AUTHOR VALUES ('sahil', 'khatti tatti');
+insert into AUTHOR VALUES ('sahil', 'tatti ki yaad');
